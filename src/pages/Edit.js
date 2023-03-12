4dd8956a3eaa57565443898b0a8d580d810a20e7
Cody Clark <codylcl@yahoo.com>
2023-03-12 11:45:22 -1000

    edit page done edit and delete route working

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,3 +1,81 @@
-export default function Edit() {
-    return <div className="text-3xl">Edit Page</div>
+import { useParams, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export default function Edit(props) {
+  const { id } = useParams();
+  const picture = props.picture.find((picture) => picture._id === id);
+  let navigate = useNavigate();
+
+  const [editForm, setEditForm] = useState(picture);
+
+  const handleChange = (event) => {
+    setEditForm((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+    console.log(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.updatePicture(editForm, picture._id);
+    console.log(editForm);
+    navigate(`/picture/${picture._id}`);
+  };
+  const removePicture = () => {
+    props.deletePicture(picture._id);
+    navigate(`/picture/`);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <p>Edit picture</p>
+        <div>
+          <div>Name:</div>
+          <input
+            type="text"
+            value={editForm.name}
+            name="name"
+            placeholder="name"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <div>Image:</div>
+          <input
+            type="text"
+            value={editForm.image}
+            name="image"
+            placeholder="image"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <div>Information:</div>
+          <input
+            type="text"
+            value={editForm.info}
+            name="info"
+            placeholder="Information"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <div>Price:</div>
+          <input
+            type="number"
+            value={editForm.price}
+            name="price"
+            placeholder="price"
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Update" />
+        </div>
+        <button onClick={() => removePicture(picture._id)}>Delete</button>
+      </form>
+    </div>
+  );
 }
