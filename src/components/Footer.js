a98a1516bd88bf9c83d0bc62f0602a4981586cea
Cody Clark <codylcl@yahoo.com>
2023-03-12 12:23:18 -1000

    basic footer styling and contact link in header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom"
+
 export default function Footer() {
-    return <div className="text-red-400 flex justify-center">
-        Footer
+    return <div className="flex justify-center space-x-40">
+        <div className="m-10 font-extralight">©Copyright Cody Clark Photography</div>
+        <Link className="flex m-10 font-extralight" to="/contact">Contact Me:</Link>
+        <img className="flex w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain-wordmark.svg" />
+        <img className="flex w-7" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
     </div>
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className="flex">
-        <div className="flex w text-1xl mx-4 my-4">Cody Clark Photography</div>
+        <Link className="flex text-1xl mx-4 my-4">Cody Clark Photography</Link>
         <div className="absolute right-0 p-4">
         <Link className="m-10" to="/">Home</Link>
         <Link className="m-10" to="/about">About</Link>
         <Link className="m-10" to="/picture">Gallery</Link>
-        {/* <Link className="p-3 m-3 m-10" to="/contact">Contact</Link> */}
+        <Link className="m-10" to="/contact">Contact</Link>
         </div>
     </div>
   );
