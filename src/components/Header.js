353c237406f45c832b5206d4f59e30a16a1322d1
Cody Clark <codylcl@yahoo.com>
2023-03-12 12:00:52 -1000

    header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="flex justify-center">
-      <div>
-        <Link className="p-3 m-3 text-green-600" to="/">Home</Link>
-        <Link className="p-3 m-3 text-green-600" to="/about">About</Link>
-        <Link className="p-3 m-3 text-green-600" to="/picture">Gallery</Link>
-        {/* <Link className="p-3 m-3 text-green-600" to="/contact">Contact</Link> */}
-      </div>
+    <div className="flex">
+        <div className="flex w text-1xl mx-4 my-4">Cody Clark Photography</div>
+        <div className="absolute right-0 p-4">
+        <Link className="m-10" to="/">Home</Link>
+        <Link className="m-10" to="/about">About</Link>
+        <Link className="m-10" to="/picture">Gallery</Link>
+        {/* <Link className="p-3 m-3 m-10" to="/contact">Contact</Link> */}
+        </div>
     </div>
   );
 }
