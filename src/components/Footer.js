93c59790b452a1c9e5f3d82a11e8c0cfe162eb9c
Cody Clark <codylcl@yahoo.com>
2023-03-12 17:30:36 -1000

    footer social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom"
 export default function Footer() {
     return <div className="flex justify-center space-x-40">
         <div className="m-10 font-extralight">©Copyright Cody Clark Photography</div>
-        <Link className="flex m-10 font-extralight" to="/contact">Contact Me:</Link>
-        <img className="flex w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain-wordmark.svg" />
-        <img className="flex w-7" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
+        <Link className="flex m-10" to="/contact">Contact Me:</Link>
+        <Link className="flex" to="https://www.linkedin.com/in/codycodes/"><img className="flex w-16" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain-wordmark.svg" /></Link>
+        <Link className="flex" to="https://github.com/codescody"><img className="flex w-7" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" /></Link>
     </div>
 }
