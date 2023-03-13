ecccdb146d8bf3b4eec75988fd187c86abfc7448
Cody Clark <codylcl@yahoo.com>
2023-03-12 17:20:37 -1000

    home page image links

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="flex">
-        <Link className="flex text-1xl mx-4 my-4" to="/">Cody Clark Photography</Link>
+    <div className="flex font-semibold">
+        <Link className="flex text-xl font-bold mx-4 my-4" to="/">Cody Clark Photography</Link>
         <div className="absolute right-0 p-4">
-        <Link className="m-10" to="/">Home</Link>
+        <Link className="m-10 " to="/">Home</Link>
         <Link className="m-10" to="/about">About</Link>
         <Link className="m-10" to="/picture">Gallery</Link>
         <Link className="m-10" to="/contact">Contact</Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
-        <div className="pt-5 pb-10 px-10"><img className="" src="https://i.imgur.com/z1nILIV.jpg"></img></div>
-        <div className="pt-5 pb-10 px-10"><img src="https://i.imgur.com/uujHS8T.jpg"></img></div>
-        <div className="pt-5 pb-10 px-10"><img src="https://i.imgur.com/kCxD1oE.jpg"></img></div>
+        <div className="pt-5 pb-10 px-10"><Link to="/picture/640e6937cf11b081d96bcf8d"><img className="" src="https://i.imgur.com/z1nILIV.jpg"></img></Link></div>
+        <div className="pt-5 pb-10 px-10"><Link to="/picture/640e6999cf11b081d96bcf91"><img src="https://i.imgur.com/uujHS8T.jpg"></img></Link></div>
+        <div className="pt-5 pb-10 px-10"><Link to="/picture/640e69edcf11b081d96bcf95"><img src="https://i.imgur.com/kCxD1oE.jpg"></img></Link></div>
     </div>
     
   );
