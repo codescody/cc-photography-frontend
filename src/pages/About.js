531c9d3a34eca21df1f7015da678f528ba97543a
Cody Clark <codylcl@yahoo.com>
2023-03-13 12:06:47 -1000

    about page basic layout new tab target for footer links

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,14 @@
 export default function About() {
-    return <div className="flex h-screen">
-    <div className="inline-flex flex-col">
+    return <div className="flex h-screen justify-center">
+    <div>
       <img
-        className="w-80 m-h-100 mx-20"
+        className="w-80 m-h-100 mt-10"
         src="https://i.imgur.com/4ZNNBPN.jpg"
       ></img>
-    </div>
-    <div className="">
+    <div className="mt-20">
         Cody Clark Photography - 
     </div>
+    </div>
+    
 </div>
 }
