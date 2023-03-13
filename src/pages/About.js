a4dccb0e353bf082d2d70cb8d97b59a75f251451
Cody Clark <codylcl@yahoo.com>
2023-03-13 12:20:38 -1000

    about page styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,13 @@
 export default function About() {
-    return <div className="flex h-screen justify-center">
+    return <div className="flex h-screen justify-center items center text-center">
     <div>
       <img
-        className="w-80 m-h-100 mt-10"
+        className="w-80 m-h-100 mt-10 ml-8 border-8 border-sky"
         src="https://i.imgur.com/4ZNNBPN.jpg"
       ></img>
-    <div className="mt-20">
-        Cody Clark Photography - 
+    <p className="w-96 mt-10 text-center leading-10">
+        Welcome to Cody Clark Photography - Over the years my passion for photography has led me to incredible experiences with amazing people. Photography has been a medium for me to express my creativity in a variety of ways. My journey in technology began with photography and I'd like to showcase some of my work with you.
+    </p>
     </div>
-    </div>
-    
 </div>
 }
