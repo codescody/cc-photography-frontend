1a0f70bcb0a3772d5b0b57aff3e9d6485918bac5
Cody Clark <codylcl@yahoo.com>
2023-03-13 14:53:33 -1000

    contact page title

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,7 @@
 export default function Contact() {
   return (
     <div className="flex flex-col h-screen justify-center text-center">
+        <h1 className="text-4xl mb-10 font-semibold">Get in touch with me</h1>
       <div className="mb-10 text-3xl">Email - Sample@mail.com</div>
       <div className="mb-10 text-3xl">Phone - 888-867-5309</div>
       <div className="mb-10 text-3xl">Instagram - @Sample</div>
